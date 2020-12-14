1ce22cfe8a947716b08b70b7352fad203debbdfd
sjjyy <sjhy013@gmail.com>
2020-12-14 17:58:31 +0900

    QA 5-1차 수정

--- app/src/main/java/com/Whowant/Tokki/UI/Activity/Mypage/MyPageActivity.java
+++ app/src/main/java/com/Whowant/Tokki/UI/Activity/Mypage/MyPageActivity.java
@@ -90,25 +90,28 @@ public class MyPageActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        if (!isPopup) {
-            String strPhoto = SimplePreference.getStringPreference(mActivity, "USER_INFO", "USER_PHOTO", "");
-
-            if (strPhoto != null && strPhoto.length() > 0 && !strPhoto.equals("null")) {
-                if (!strPhoto.startsWith("http"))
-                    strPhoto = CommonUtils.strDefaultUrl + "images/" + strPhoto;
-
-                Glide.with(mActivity)
-                        .asBitmap() // some .jpeg files are actually gif
-                        .placeholder(R.drawable.user_icon)
-                        .load(strPhoto)
-                        .apply(new RequestOptions().circleCrop())
-                        .into(photoIv);
-            } else {
-                photoIv.setImageResource(R.drawable.user_icon);
-            }
-
-            initData();
+        if (isPopup) {
+            isPopup = false;
+            return;
         }
+
+        String strPhoto = SimplePreference.getStringPreference(mActivity, "USER_INFO", "USER_PHOTO", "");
+
+        if (strPhoto != null && strPhoto.length() > 0 && !strPhoto.equals("null")) {
+            if (!strPhoto.startsWith("http"))
+                strPhoto = CommonUtils.strDefaultUrl + "images/" + strPhoto;
+
+            Glide.with(mActivity)
+                    .asBitmap() // some .jpeg files are actually gif
+                    .placeholder(R.drawable.user_icon)
+                    .load(strPhoto)
+                    .apply(new RequestOptions().circleCrop())
+                    .into(photoIv);
+        } else {
+            photoIv.setImageResource(R.drawable.user_icon);
+        }
+
+        initData();
     }
 
     private void initView() {
